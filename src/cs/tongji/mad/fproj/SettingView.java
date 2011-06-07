dbcfac20796a3b097a4fe45beb9f9a717b580b75
qixiaochen@gmail.com <qixiaochen@gmail.com@5d9a875e-b1a7-14b0-4603-314cddc6b85c>
2011-06-07 16:04:06 +0000

    final?

--- src/cs/tongji/mad/fproj/SettingView.java
+++ src/cs/tongji/mad/fproj/SettingView.java
@@ -7,13 +7,24 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
@@ -33,42 +44,43 @@ public class SettingView extends Activity {
 	public static final int CONNECT_END = 11;
 	public Handler handler;
 	private Communicator communicator;
-	
-	private class SettingViewHandler extends Handler{
+
+	private class SettingViewHandler extends Handler {
 		public void handleMessage(Message msg) {
 			switch (msg.what) {
 			case SEARCH_END:
 				@SuppressWarnings("unchecked")
-				Set<String> findResult = (Set<String>)msg.obj;
+				Set<String> findResult = (Set<String>) msg.obj;
 				List<String> deviceNames = new ArrayList<String>();
 				deviceNames.addAll(findResult);
-				TextView textViewHostCount =
-					(TextView)SettingView.this.findViewById(R.id.textViewHostCount);
+				TextView textViewHostCount = (TextView) SettingView.this
+						.findViewById(R.id.textViewHostCount);
 				if (deviceNames.size() == 0) {
 					textViewHostCount.setText(R.string.no_host_found);
 				} else {
 					String hostCount = Integer.toString(deviceNames.size());
-					hostCount += " " + SettingView.this.getText(R.string.host_found);
+					hostCount += " "
+							+ SettingView.this.getText(R.string.host_found);
 					textViewHostCount.setText(hostCount);
 				}
-				Button btnSearch = 
-					(Button)SettingView.this.findViewById(R.id.buttonSearch);
+				Button btnSearch = (Button) SettingView.this
+						.findViewById(R.id.buttonSearch);
 				btnSearch.setText(R.string.search_button);
 				btnSearch.setEnabled(true);
-				ProgressBar progressBarSearch = 
-					(ProgressBar)SettingView.this.findViewById(R.id.progressBarSearch);
+				ProgressBar progressBarSearch = (ProgressBar) SettingView.this
+						.findViewById(R.id.progressBarSearch);
 				progressBarSearch.setVisibility(View.INVISIBLE);
-				ListView listViewSearchResult = 
-					(ListView)findViewById(R.id.listViewSearchResult);
+				ListView listViewSearchResult = (ListView) findViewById(R.id.listViewSearchResult);
 				listViewSearchResult.setAdapter(new ArrayAdapter<String>(
-						SettingView.this,android.R.layout.simple_expandable_list_item_1,
+						SettingView.this,
+						android.R.layout.simple_expandable_list_item_1,
 						deviceNames));
-				//progressBar1.setIndeterminate(false);
+				// progressBar1.setIndeterminate(false);
 				break;
-			
+
 			case CONNECT_END:
 				String ErrorMessage;
-				switch ((Integer)msg.obj) {
+				switch ((Integer) msg.obj) {
 				case Communicator.ERROR_SUCCESS:
 					Intent intent = new Intent();
 					intent.setClass(SettingView.this, GontrolMain.class);
@@ -76,58 +88,62 @@ public class SettingView extends Activity {
 					SettingView.this.finish();
 					return;
 				case Communicator.ERROR_NO_RESPONSE:
-					Log.v("Connect","No response");
-					ErrorMessage = 
-						SettingView.this.getText(R.string.no_response).toString();
+					Log.v("Connect", "No response");
+					ErrorMessage = SettingView.this.getText(
+							R.string.no_response).toString();
 					break;
 				case Communicator.ERROR_HOSTNAME_UNKNOWN:
-					ErrorMessage = 
-						SettingView.this.getText(R.string.host_unknown).toString();
+					ErrorMessage = SettingView.this.getText(
+							R.string.host_unknown).toString();
 					break;
 				default:
-					ErrorMessage = 
-						SettingView.this.getText(R.string.error_unknown).toString();
+					ErrorMessage = SettingView.this.getText(
+							R.string.error_unknown).toString();
 					break;
 				}
-				Toast.makeText(SettingView.this, ErrorMessage, Toast.LENGTH_LONG).show();
+				Toast.makeText(SettingView.this, ErrorMessage,
+						Toast.LENGTH_LONG).show();
 				break;
 			}
 			super.handleMessage(msg);
 		}
 	}
-	
+
 	private class SearchThread extends Thread {
 		public void run() {
-			DatagramSocket findSocket = SettingView.this.communicator.searchHost();
+			DatagramSocket findSocket = SettingView.this.communicator
+					.searchHost();
 			try {
 				TimeUnit.MILLISECONDS.sleep(5000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			//findThread.
+			// findThread.
 			findSocket.close();
-			
+
 			Message msg = new Message();
 			msg.what = SEARCH_END;
 			msg.obj = SettingView.this.communicator.getSearchHostResult();
 			handler.sendMessage(msg);
 		}
 	}
-	
+
 	private class ConnectThread extends Thread {
 		private String hostname;
 		private ProgressDialog prgrssDialog;
+
 		public ConnectThread(String hostname, ProgressDialog prgrssDialog) {
 			this.hostname = hostname;
 			this.prgrssDialog = prgrssDialog;
 		}
+
 		public void run() {
-			int result = communicator.connect(hostname);	
+			int result = communicator.connect(hostname);
 			prgrssDialog.dismiss();
 			Message msg = new Message();
 			msg.what = CONNECT_END;
 			msg.obj = new Integer(result);
-			Log.v("Connect","End Post");
+			Log.v("Connect", "End Post");
 			handler.sendMessage(msg);
 		}
 	}
@@ -144,38 +160,34 @@ public class SettingView extends Activity {
 		@Override
 		public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 				long arg3) {
-			TextView textViewItem = (TextView)arg1;
+			TextView textViewItem = (TextView) arg1;
 			SettingView.this.connect(textViewItem.getText().toString());
 			Log.v("Item", textViewItem.getText().toString());
-			
+
 			// TODO Auto-generated method stub
-			
+
 		}
 	}
-	
-	
+
 	private class CustomizeHostConnectClickListener implements OnClickListener {
 		@Override
 		public void onClick(View arg0) {
-			EditText editTextHostName = (EditText)
-			SettingView.this.findViewById(R.id.editTextCustomizeHostName);
+			EditText editTextHostName = (EditText) SettingView.this
+					.findViewById(R.id.editTextCustomizeHostName);
 			String hostName = editTextHostName.getText().toString();
 			if (hostName == null || hostName.equals("")) {
 				Toast.makeText(SettingView.this, R.string.hostname_empty,
 						Toast.LENGTH_SHORT).show();
-			}
-			else
-			{
-				Log.v("Host",hostName);
-				InputMethodManager imm = (InputMethodManager)
-				getSystemService(Context.INPUT_METHOD_SERVICE);
-				imm.hideSoftInputFromWindow(editTextHostName.getWindowToken(), 0);
+			} else {
+				Log.v("Host", hostName);
+				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(editTextHostName.getWindowToken(),
+						0);
 				SettingView.this.connect(hostName);
 			}
 		}
 	}
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -183,37 +195,72 @@ public class SettingView extends Activity {
 		this.setContentView(R.layout.setting);
 		communicator = WifiComm.getInstance();
 		handler = new SettingViewHandler();
-		Button btnSearch = (Button)findViewById(R.id.buttonSearch);
+		Button btnSearch = (Button) findViewById(R.id.buttonSearch);
 		btnSearch.setOnClickListener(new SearchClickListener());
-		Button btnCustomizeHostConnect = 
-			(Button)findViewById(R.id.buttonCustomizeHostConnect);
-		btnCustomizeHostConnect.setOnClickListener(
-				new CustomizeHostConnectClickListener());
-		ListView listViewSearchResult = 
-			(ListView)findViewById(R.id.listViewSearchResult);
-		listViewSearchResult.setOnItemClickListener(
-				new SearchResultItemClickListener());
+		Button btnCustomizeHostConnect = (Button) findViewById(R.id.buttonCustomizeHostConnect);
+		btnCustomizeHostConnect
+				.setOnClickListener(new CustomizeHostConnectClickListener());
+		ListView listViewSearchResult = (ListView) findViewById(R.id.listViewSearchResult);
+		listViewSearchResult
+				.setOnItemClickListener(new SearchResultItemClickListener());
 		search();
 	}
 
 	private void search() {
-		Button btnSearch = (Button)findViewById(R.id.buttonSearch);
+		Button btnSearch = (Button) findViewById(R.id.buttonSearch);
 		btnSearch.setText(R.string.searching_button);
 		btnSearch.setEnabled(false);
-		ProgressBar progressBarSearch = 
-			(ProgressBar)SettingView.this.findViewById(R.id.progressBarSearch);
+		ProgressBar progressBarSearch = (ProgressBar) SettingView.this
+				.findViewById(R.id.progressBarSearch);
 		progressBarSearch.setVisibility(View.VISIBLE);
-		new SearchThread().start();		
+		new SearchThread().start();
 	}
-	
+
 	private void connect(String hostname) {
-		ProgressDialog dialog=new ProgressDialog(this);
+		ProgressDialog dialog = new ProgressDialog(this);
 		dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-		//dialog.setIcon(android.R.drawable.ic_dialog_info);
+		// dialog.setIcon(android.R.drawable.ic_dialog_info);
 		dialog.setTitle(R.string.please_wait);
 		dialog.setMessage(getText(R.string.connecting_host).toString());
 		dialog.setCancelable(false);
 		dialog.show();
-		new ConnectThread(hostname,dialog).start();
+		new ConnectThread(hostname, dialog).start();
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		// TODO Auto-generated method stub
+		super.onCreateContextMenu(menu, v, menuInfo);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.aboutmenu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.item1:
+			AlertDialog ad = new AlertDialog.Builder(this)
+					.setTitle(R.string.aboutitle)
+					.setMessage(Html.fromHtml((String) getResources()
+							.getText(R.string.allsite)))
+					.setPositiveButton(R.string.confirm,
+							new DialogInterface.OnClickListener() {
+								public void onClick(
+										DialogInterface dialoginterface, int i) {
+								}
+							}).create();
+			ad.show();
+			((TextView) ad.findViewById(android.R.id.message))
+					.setMovementMethod(LinkMovementMethod.getInstance());
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
